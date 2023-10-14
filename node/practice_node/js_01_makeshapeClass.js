d488a6484e164dcd6a98d4212fe68c77f5a71c57
Siri Lee <eunsilling@gmail.com>
2023-10-14 17:52:22 +0900

    실습연습듕

---
+++ node/practice_node/js_01_makeshapeClass.js
@@ -0,0 +1,20 @@
+class Area {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+let area1 = new Area(1, 2);
+console.log(`넓이: `, area1.getArea());
+
+class Rectangle {}
+class Triangle {}
+class Circle {}
+// 전체 클래스 상속
+console.log(
+  `직사각형 넓이: ${rec1.getArea()}, 삼각형 넓이: ${tri1.getArea()}, 원 넓이: ${cir1.getArea()}`
+);
