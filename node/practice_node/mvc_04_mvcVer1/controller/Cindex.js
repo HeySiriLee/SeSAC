c0553cfc9cbd1280903eac6d185a2893521e740b
Siri Lee <eunsilling@gmail.com>
2023-10-27 18:26:50 +0900

    실습연습듕

--- node/practice_node/mvc_04_mvcVer1/controller/Cindex.js
+++ node/practice_node/mvc_04_mvcVer1/controller/Cindex.js
@@ -6,23 +6,33 @@ exports.loginPage = (req, res) => {
 
 exports.index = (req, res) => {
   let userData = users();
+  let idMatched = false;
+  let pwMatched = false;
 
-  let msg = "";
   for (let i = 0; i < userData.length; i++) {
     if (req.body.userId == userData[i].userId) {
+      idMatched = true;
       if (req.body.userPw == userData[i].userPw) {
-        msg = "Success Log in:)";
+        pwMatched = true;
         break;
-      } else {
-        msg = "Wrong Password:(";
       }
-    } else {
-      msg = "Wrong ID:(";
     }
   }
 
+  let msg = "";
+
+  if (idMatched && pwMatched) {
+    msg = `Success Log in, Welcome ${req.body.userId}!`;
+  } else if (idMatched) {
+    msg = "Wrong Password:(";
+  } else if (pwMatched) {
+    msg = "Wrong ID:(";
+  } else {
+    msg = "Check your ID and Password:(";
+  }
+
   res.send({
-    isSuccess: msg === "Success Log in:)",
-    msg: `Success Log in, Welcome ${req.body.userId}!`,
+    isSuccess: msg === "Success Log in, Welcome " + req.body.userId + "!",
+    msg,
   });
 };
