2d4994560b6025c06369463234dae68d946a44eb
Siri Lee <eunsilling@gmail.com>
2023-10-30 15:42:54 +0900

    강의정리노션노정리

--- node/11_mvc-mysql/routes/index.js
+++ node/11_mvc-mysql/routes/index.js
@@ -9,8 +9,13 @@ router.get("/visitors", controller.visitor);
 // 방명록 등록
 router.post("/visitor", controller.postVisitor);
 
-router.patch("/visitor/:id", (req, res) => {});
+// 방명록 수정
+router.patch("/visitor", controller.patchVisitor);
 
+// 방명록 하나 조회
+router.get("/visitor/:id", controller.getVisitorById);
+
+// 방명록 삭제
 router.delete("/visitor/:id", controller.deleteVisitor);
 
 module.exports = router;
