b13f413dd4c3ee78fafc4bb440d0c679fb4c8439
Siri Lee <eunsilling@gmail.com>
2023-11-28 17:41:04 +0900

    실습연습듕

--- react/practice_react/01_jsx/src/Components/Component.js
+++ react/practice_react/01_jsx/src/Components/Component.js
@@ -8,26 +8,34 @@ function Component() {
   const b = 10;
 
   return (
-    <div>
-      <img src="./개냥.png" alt="개냥2" />
+    <div style={{ textAlign: "center" }}>
+      <br />
+      <img
+        src="./개냥.png"
+        alt="개냥2"
+        style={{ width: "25%", height: "25%" }}
+      />
       <h3>제 반려 동물의 이름은 {name}입니당</h3>
       <h3>
         {name}은 {animal}입니당 겹죵?
       </h3>
-
       <hr />
-
-      {sum === 8 ? "Than's right!:)" : "Oh, Can u try again?"}
-
+      {sum === 8 ? "That's right!:)" : "Oh, Can u try again?"}
       <hr />
-
       {a > b && "a는 b보다 큽니당"}
-
       <hr />
-
-      <div className="title">로그인을 해듀세욥</div>
-      <input>Id: </input>
-      <input>Password: </input>
+      <div className="title" style={{ justifyContent: "center" }}>
+        로그인을 해듀세욥
+      </div>
+      <div>
+        Id: <input />
+      </div>
+      <br />
+      <div>
+        Password:
+        <input />
+      </div>
+      <br />
       <button
         onClick={() => {
           console.log("click!");
