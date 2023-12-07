3d418303deb791f8c213afb4e11f96a243f42d89
Siri Lee <eunsilling@gmail.com>
2023-12-07 15:27:26 +0900

    실습연습듕

---
+++ react/practice_react/05_style/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Larva from "./components/Larva";
+
+function App() {
+  return (
+    <div className="App">
+      <Larva />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/practice_react/05_style/src/components/Larva.js
@@ -0,0 +1,24 @@
+import "../styles/larva.scss";
+
+function Larva() {
+  return (
+    <div className="larva">
+      <div className="body head">
+        <div className="eye1"></div>
+        <div className="eye2"></div>
+      </div>
+      <div className="body body1"></div>
+      <div className="body body2"></div>
+      <div className="body body3"></div>
+      <div className="body tail"></div>
+
+      <img
+        className="grass"
+        src={process.env.PUBLIC_URL + "/grass.png"}
+        alt="잔디"
+      />
+    </div>
+  );
+}
+
+export default Larva;
